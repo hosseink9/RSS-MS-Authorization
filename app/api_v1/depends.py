c518f6d88ccca661a54471855ecb061da5e0add1
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-02 15:33:23 +0330

    Create get current user func for depends user

--- app/api_v1/depends.py
+++ app/api_v1/depends.py
@@ -21,3 +21,32 @@ reuseable_oauth = OAuth2PasswordBearer(
 )
 
 
+async def get_current_user(token: str = Depends(reuseable_oauth)):
+    try:
+
+        payload = jwt.decode(
+            token, JWT_SECRET_KEY, algorithms=[ALGORITHM]
+        )
+
+        if datetime.fromtimestamp(payload['exp']) < datetime.now():
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user: Dict[str, Any] | None = users_collection.find_one(
+        {'username': payload['sub']})
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Could not find user",
+        )
+
+    return user
