4a939ed5b611d76a4d76f08cbfa24bfaeacad0ef
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-04 23:37:37 +0330

    Add a new schema for otp

--- app/schema/schemas.py
+++ app/schema/schemas.py
@@ -4,23 +4,27 @@ from pydantic import BaseModel
 class UserRequest(BaseModel):
     username: str
     password: str
-    firstname: str | None=None
-    lastname: str | None=None
+    firstname: str | None = None
+    lastname: str | None = None
 
 
 class UserResponse(BaseModel):
     id: str
     username: str
-    firstname: str | None=None
-    lastname: str | None=None
-
+    firstname: str | None = None
+    lastname: str | None = None
 
 
 class UserUpdate(BaseModel):
-    username: str | None=None
-    firstname: str | None=None
-    lastname: str | None=None
+    username: str | None = None
+    firstname: str | None = None
+    lastname: str | None = None
+
 
 class TokenResponse(BaseModel):
     access_token: str
     refresh_token: str
+
+
+class Otp(BaseModel):
+    otp: str
