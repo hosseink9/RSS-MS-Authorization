d6b9e7d71acf46e27952343023bf597404124333
hosseink9 <hosseinkalantari.29@gmail.com>
2023-11-07 23:23:46 +0330

    Add middleware for our app

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
+import logging
+import sys
 from fastapi import FastAPI
-from .api_v1.endpoints.routers import router as authorization_routes
+
+from app.log.logging_lib import RouterLoggingMiddleware
+from app.api_v1.endpoints.routers import router as authorization_routes
+from app.core.config import logging_config
+
+logging.config.dictConfig(logging_config)
 
 app = FastAPI()
+app.add_middleware(RouterLoggingMiddleware,
+                   logger=logging.getLogger("elastic-logger"))
 app.include_router(router=authorization_routes, prefix="")
